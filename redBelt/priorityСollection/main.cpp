2b372f9d119e55c13524bebe2b18300b825a538a
Oleg Kozyrev <kozyrev.ov@edu.spbstu.ru>
2021-03-22 16:33:27 +0300

    add: new project priorityСollection

---
+++ redBelt/priorityСollection/main.cpp
@@ -0,0 +1,70 @@
+﻿#include "testRunner.h"
+#include "priorityСollection.h"
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <set>
+
+class StringNonCopyable : public string
+{
+public:
+	using string::string;  // Позволяет использовать конструкторы строки
+	StringNonCopyable(const StringNonCopyable&) = delete;
+	StringNonCopyable(StringNonCopyable&&) = default;
+	StringNonCopyable& operator=(const StringNonCopyable&) = delete;
+	StringNonCopyable& operator=(StringNonCopyable&&) = default;
+};
+
+void TestNoCopy()
+{
+	PriorityCollection<StringNonCopyable> strings;
+	const auto white_id = strings.Add("white");
+	const auto yellow_id = strings.Add("yellow");
+	const auto red_id = strings.Add("red");
+
+	strings.Promote(yellow_id);
+	for (int i = 0; i < 2; ++i) 
+	{
+		strings.Promote(red_id);
+	}
+
+	strings.Promote(yellow_id);
+
+	{
+		const auto item = strings.PopMax();
+		ASSERT_EQUAL(item.first, "red");
+		ASSERT_EQUAL(item.second, 2);
+	}
+	
+	{
+		const auto item = strings.PopMax();
+		ASSERT_EQUAL(item.first, "yellow");
+		ASSERT_EQUAL(item.second, 2);
+	}
+	
+	{
+		const auto item = strings.PopMax();
+		ASSERT_EQUAL(item.first, "white");
+		ASSERT_EQUAL(item.second, 0);
+	}
+}
+
+int main()
+{
+	TestRunner tr;
+	RUN_TEST(tr, TestNoCopy);
+	/*vector<pair<priority, val>> tmp2;
+	set<pair<priority, id>> tmp;
+
+	tmp.insert(make_pair(1, 7));
+	tmp.insert(make_pair(4, 2));
+	tmp.insert(make_pair(2, 7));
+	tmp.insert(make_pair(-3, 3));
+
+	for(auto& t : tmp)
+		cout << t.first << ' ' << t.second << endl;*/
+
+	return 0;
+}
---
+++ redBelt/priorityСollection/priorityСollection.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+class PriorityCollection
+{
+public:
+	using Id = int;
+
+	// Добавить объект с нулевым приоритетом
+	// с помощью перемещения и вернуть его идентификатор
+	Id Add(T object);
+
+	// Добавить все элементы диапазона [range_begin, range_end)
+	// с помощью перемещения, записав выданные им идентификаторы
+	// в диапазон [ids_begin, ...)
+	template <typename ObjInputIt, typename IdOutputIt>
+	void Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin);
+
+	// Определить, принадлежит ли идентификатор какому-либо
+	// хранящемуся в контейнере объекту
+	bool IsValid(Id id) const;
+
+	// Получить объект по идентификатору
+	const T& Get(Id id) const;
+
+	// Увеличить приоритет объекта на 1
+	void Promote(Id id);
+
+	// Получить объект с максимальным приоритетом и его приоритет
+	pair<const T&, int> GetMax() const;
+
+	// Аналогично GetMax, но удаляет элемент из контейнера
+	pair<T, int> PopMax();
+
+private:
+	vector<pair<int, T>> objects;
+	set<pair<int, Id>, greater<int>> objectsSortByPriority;
+	//vector<pair<priority, val>> tmp2;
+	//set<pair<priority, id>> tmp;
+};
+
+template <typename T>
+typename PriorityCollection<T>::Id PriorityCollection<T>::Add(T object)
+{
+	objects.push_back(make_pair(0, move(object)));
+	objectsSortByPriority.insert(make_pair(0, objects.size() - 1));
+	return objects.size() - 1;
+}
+
+template <typename T>
+template <typename ObjInputIt, typename IdOutputIt>
+void PriorityCollection<T>::Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin)
+{
+	for(auto& it = range_begin; it < range_end; ++it)
+	{
+		objects.push_back(make_pair(0, move(*it)));
+		objectsSortByPriority.insert(make_pair(0, objects.size() - 1));
+		ids_begin++ = objects.size() - 1;
+	}
+}
+
+template <typename T>
+bool PriorityCollection<T>::IsValid(Id id) const
+{
+	return objects.size() > id;
+}
+
+template <typename T>
+const T& PriorityCollection<T>::Get(Id id) const
+{
+	return objects[id].second;
+}
+
+template <class T1, class T2>
+bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y)
+{
+    return x.second < y.second;
+}
+
+template <typename T>
+void PriorityCollection<T>::Promote(Id id)
+{
+	++objects[id].first;
+
+	auto it = find(objectsSortByPriority.begin(), objectsSortByPriority.end(), 
+		[id](const pair<int, Id>& element)
+		{
+		   return element.second == id;
+		});
+
+	//it->first++;
+}
+
+template <typename T>
+pair<const T&, int> PriorityCollection<T>::GetMax() const
+{
+	return make_pair(objects[objectsSortByPriority.begin()->second].second, objectsSortByPriority.begin()->first);
+}
+
+template <typename T>
+pair<T, int> PriorityCollection<T>::PopMax()
+{
+	auto maxElementByPriority = move(*objectsSortByPriority.begin());
+	auto maxElement = move(objects[maxElementByPriority.second].second);
+	objectsSortByPriority.erase(objectsSortByPriority.begin());
+	objects.erase(objects.begin() + maxElementByPriority.second);
+	
+	return make_pair(maxElement, maxElementByPriority.first);
+}
