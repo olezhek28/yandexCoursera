2a6ee8cbf239be624d472e13c847c8cd06cbe035
Oleg Kozyrev <kozyrev.ov@edu.spbstu.ru>
2021-04-13 09:50:07 +0300

    add slow solution

--- redBelt/e-book/e-book.cpp
+++ redBelt/e-book/e-book.cpp
@@ -1,20 +1,141 @@
-// e-book.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <iomanip>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+class ReadingManager
+{
+public:
+	ReadingManager();
+
+	void Read(int user_id, int page_count);
+	double Cheer(int user_id) const;
+
+private:
+	// —татическое поле не принадлежит какому-то конкретному
+	// объекту класса. ѕо сути это глобальна€ переменна€,
+	// в данном случае константна€.
+	// Ѕудь она публичной, к ней можно было бы обратитьс€ снаружи
+	// следующим образом: ReadingManager::MAX_USER_COUNT.
+	static const int MAX_USER_COUNT_ = 100'000;
+
+	vector<int> user_page_counts_;
+	vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
+	vector<int> user_positions_; // позиции в векторе sorted_users_
+
+	int GetUserCount() const;
+	void AddUser(int user_id);
+	void SwapUsers(int lhs_position, int rhs_position);
+};
+
+ReadingManager::ReadingManager(): user_page_counts_(MAX_USER_COUNT_ + 1, 0),
+									sorted_users_(),
+									user_positions_(MAX_USER_COUNT_ + 1, -1)
+{
+}
+
+void ReadingManager::Read(int user_id, int page_count)
+{
+	if (user_page_counts_[user_id] == 0)
+	{
+		AddUser(user_id);
+	}
+
+	user_page_counts_[user_id] = page_count;
+	int& position = user_positions_[user_id];
+
+	while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]])
+	{
+		SwapUsers(position, position - 1);
+	}
+}
+
+double ReadingManager::Cheer(int user_id) const
+{
+	if (user_page_counts_[user_id] == 0)
+	{
+		return 0;
+	}
+
+	const int user_count = GetUserCount();
+	if (user_count == 1)
+	{
+		return 1;
+	}
+
+	const int page_count = user_page_counts_[user_id];
+	int position = user_positions_[user_id];
+
+	while (position < user_count &&
+		user_page_counts_[sorted_users_[position]] == page_count)
+	{
+		++position;
+	}
+
+	if (position == user_count)
+	{
+		return 0;
+	}
+	// ѕо умолчанию деление целочисленное, поэтому
+	// нужно привести числитель к типу double.
+	// ѕростой способ сделать это Ч умножить его на 1.0.
+	return (user_count - position) * 1.0 / (user_count - 1);
+}
+
+int ReadingManager::GetUserCount() const
+{
+	return sorted_users_.size();
+}
+
+void ReadingManager::AddUser(int user_id)
+{
+	sorted_users_.push_back(user_id);
+	user_positions_[user_id] = sorted_users_.size() - 1;
+}
+
+void ReadingManager::SwapUsers(int lhs_position, int rhs_position)
+{
+	const int lhs_id = sorted_users_[lhs_position];
+	const int rhs_id = sorted_users_[rhs_position];
+	swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
+	swap(user_positions_[lhs_id], user_positions_[rhs_id]);
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// ƒл€ ускорени€ чтени€ данных отключаетс€ синхронизаци€
+	// cin и cout с stdio,
+	// а также выполн€етс€ отв€зка cin от cout
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	ReadingManager manager;
+
+	int query_count;
+	cin >> query_count;
+
+	for (int query_id = 0; query_id < query_count; ++query_id) 
+	{
+		string query_type;
+		cin >> query_type;
+		int user_id;
+		cin >> user_id;
+
+		if (query_type == "READ") 
+		{
+			int page_count;
+			cin >> page_count;
+			manager.Read(user_id, page_count);
+		}
+		else if (query_type == "CHEER") 
+		{
+			cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+		}
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
